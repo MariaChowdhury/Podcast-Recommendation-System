c1a035c7bc82b68f3a5b78ffd34617f66deff1bf
MariaChowdhury <maria.chowdhury@gmail.com>
2018-02-03 15:25:13 -0800

    Issue 3 completed

---
+++ history.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  2 13:49:48 2018
+
+@author: mariachowdhury
+
+This script returns the watched episode by all users
+"""
+import os
+import json
+user_history_file=os.getcwd()+"/private/"+"history.json"
+user_episodes_file=os.getcwd()+"/private/"+"user_episodes.txt"
+
+json_file=open(user_history_file,"r")
+strings = json.load(json_file)
+json_file.close()
+
+    
+"""collecting list of users and watched episodes in the .json files"""
+user_episodes=[]
+for key,val in strings.items():
+    for k,v in val.items():
+        user_key=key+","+k
+        user_episodes.append(user_key)
+            
+        
+"""writing output file that inclueds all users and watched episodes"""
+    
+if os.path.exists(user_episodes_file):
+    os.remove(user_episodes_file)
+
+user_episodes_write=open(user_episodes_file,"w")
+for v in user_episodes:
+    user_episodes_write.write(v+ "\n")
+   
+        
